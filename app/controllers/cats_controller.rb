ca41db7c2b1dd659e670e60eb792daae5c791c5b
nelmarianna <aniela.opolski@gmail.com>
2016-04-26 12:02:07 -0400

    CRUD complete

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,4 +1,6 @@
 class CatsController < ApplicationController
+	
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@cats = Cat.all
@@ -9,23 +11,43 @@ class CatsController < ApplicationController
 	end
 
 	def create
-		@cat = Cat.create(cat_params)
-		redirect_to cats_path
+		if @cat = Cat.create(cat_params)
+			flash[:success]="Now everyone can check out your pussy"
+			redirect_to cats_path
+		else
+			flash.now[:alert]="Oh No! Your cat ran away, try again"
+			render :new
 	end
 
 	def show
-		@cat = Cat.find(params[:id])
+		
 	end
-	
+
 	def edit
+		
 	end
 
 	def update
+		if @cat.update(cat_params)
+			flash[:success]="You've successfully updated your \"cat\"tion *slowclap*."
+		redirect_to cat_path(@cat)
+	else
+		flash.now[:alert]="No one can see that you updated that typo and now everyone thinks you can't spell.... try again"
+		render :edit
+	end
+
+	def destroy
+		@cat.destroy
+		redirect_to cats_path
 	end
 
 	private
 
 	def cat_params
 		params.require(:cat).permit(:image, :caption)
+	end
+
+	def set_post
+		@cat = Cat.find(params[:id])
 	end
 end
